508df17295ef4c537246fa5e8d3e1282953e47d6
眞田悟司 <84009331+ss473901@users.noreply.github.com>
2022-02-07 02:10:27 +0900

    Complete Atoms , molecules design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "さなだん",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "123456@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
